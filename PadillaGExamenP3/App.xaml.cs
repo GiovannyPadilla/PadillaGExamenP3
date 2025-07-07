a555ecd557d881bea567b8db115595c6de54620c
GiovannyPadilla <146996637+GiovannyPadilla@users.noreply.github.com>
2025-07-07 07:13:21 -0500

    Agregar archivos de proyecto.

---
+++ PadillaGExamenP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PadillaGExamenP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
